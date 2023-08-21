e26040ec2b5b90d1db6c03d5dc3ad221b5c1b0d6
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-08-21 21:42:32 +0200

    initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import logo from "../assets/images/logo1.png";
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img src={logo} alt="jobster logo" className="logo" />
+    </div>
+  );
+};
+export default Logo;
